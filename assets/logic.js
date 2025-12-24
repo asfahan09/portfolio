ccd0f70093453708d72e0466e10daa9c546db0f0
Asfahan-hub <asfahan.pbhub022@gmail.com>
2025-12-24 11:43:17 -0800

    portfolio complete

---
+++ assets/logic.js
@@ -0,0 +1,28 @@
+
+
+function sendbtn() {
+  var param ={
+    name:document.getElementById("full_name").value,
+    email:document.getElementById("email").value,
+    phone:document.getElementById("phone").value,
+    message:document.getElementById("message").value,
+
+  };
+
+  const serviceId="service_0mzo57b";
+  const templateId="template_frnm7zc";
+
+  emailjs
+  .send(serviceId,templateId,param)
+  .then((res)=>{
+    document.getElementById("full_name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Your Message Send!")
+  })
+  .catch((err)=>console.log(err));
+
+  
+}
